ce8d9522a6cf05521374f3cd075058966e5adb88
Philippe Delebarre <pdelebarre@gmail.com>
2024-08-30 17:10:25 +0200

    conf upd

--- vitest.config.js
+++ vitest.config.js
@@ -1,13 +1,13 @@
-import { defineConfig } from "vite";
+import { defineConfig } from "vitest/config";
 
 export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: ["./vitest.setup.ts"],
+    reporters: [["vitest-sonar-reporter", { outputFile: "sonar-report.xml" }]],
+
+    coverage: {
+      reporters: "lcov",
+    },
   },
-  plugins: [
-    createReporter({
-      reporters: ["lcov", "text"],
-    }),
-  ],
 });
